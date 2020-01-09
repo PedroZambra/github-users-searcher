707c805fd953e677ee744403e29228a14259c8c0
PedroZambra <pezamesp@gmail.com>
2020-01-09 13:59:24 +0100

    searcher

---
+++ app.js
@@ -0,0 +1,36 @@
+
+document.getElementById("search").addEventListener('click', search);
+
+function search(){
+    user = document.getElementById("user").value;
+    fetch('https://api.github.com/users/'+user)
+    .then( res => res.json())
+    .then ( data => {
+        if(data.message == "Not Found") {
+            document.getElementById("data").innerText = "Usuario no encontrado";
+            document.getElementById("user").value = "";
+        } else {
+            fetch('https://api.github.com/users/'+ user +'/events')
+            .then( res => res.json())
+            .then( data2 => {
+                for(let i=1; i<=30; i++){
+                    if(data2[i].type === "PushEvent"){
+                        var push = i;
+                        break;
+                    }
+                }
+                document.getElementById("data").innerHTML = "";
+                document.getElementById("data").innerHTML +=    `<div class="user">
+                                                                    <h3>${data.login}</h3>
+                                                                    <img src=${data2[0].actor.avatar_url}>
+                                                                    <div class="info">
+                                                                        <p>Name: ${data.name}</p>
+                                                                        ${(data.location !== null ? `<p>Location: ${data.location}</p>` : null )}
+                                                                        <p>Email: ${data2[push].payload.commits[0].author.email}</p>
+                                                                    </div>    
+                                                                </div>`            
+            })
+        }
+    })
+    .catch( err => console.log(err))
+}
